40c24802986b14d7123c68af80a14382347e9e26
Christoph Hermann <schtoeffel@gmail.com>
2013-08-07 08:19:29 +0200

    vundle

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,47 +3,47 @@ require homebrew
 require gcc
 
 Exec {
-  group       => 'staff',
-  logoutput   => on_failure,
-  user        => $luser,
+    group       => 'staff',
+                logoutput   => on_failure,
+                user        => $luser,
 
-  path => [
-    "${boxen::config::home}/rbenv/shims",
-    "${boxen::config::home}/rbenv/bin",
-    "${boxen::config::home}/rbenv/plugins/ruby-build/bin",
-    "${boxen::config::home}/homebrew/bin",
-    '/usr/bin',
-    '/bin',
-    '/usr/sbin',
-    '/sbin'
-  ],
+                path => [
+                    "${boxen::config::home}/rbenv/shims",
+                "${boxen::config::home}/rbenv/bin",
+                "${boxen::config::home}/rbenv/plugins/ruby-build/bin",
+                "${boxen::config::home}/homebrew/bin",
+                '/usr/bin',
+                '/bin',
+                '/usr/sbin',
+                '/sbin'
+                    ],
 
-  environment => [
-    "HOMEBREW_CACHE=${homebrew::config::cachedir}",
-    "HOME=/Users/${::luser}"
-  ]
+                environment => [
+                    "HOMEBREW_CACHE=${homebrew::config::cachedir}",
+                "HOME=/Users/${::luser}"
+                    ]
 }
 
 File {
-  group => 'staff',
-  owner => $luser
+    group => 'staff',
+          owner => $luser
 }
 
 Package {
-  provider => homebrew,
-  require  => Class['homebrew']
+    provider => homebrew,
+             require  => Class['homebrew']
 }
 
 Repository {
-  provider => git,
-  extra    => [
-    '--recurse-submodules'
-  ],
-  require  => Class['git']
+    provider => git,
+             extra    => [
+                 '--recurse-submodules'
+                 ],
+             require  => Class['git']
 }
 
 Service {
-  provider => ghlaunchd
+    provider => ghlaunchd
 }
 
 Homebrew::Formula <| |> -> Package <| |>
@@ -51,251 +51,251 @@ Homebrew::Formula <| |> -> Package <| |>
 
 
 node default {
-  # core modules, needed for most things
-  
-  include git
-  git::config::global { 'user.email':
-    value  => 'schtoeffel@gmail.com'
-  }
-  file { "/Users/${::boxen_user}/.gitconfig":
-    target  => "/Users/${::boxen_user}/.dotfiles/gitconfig",
-    require => Repository["/Users/${::boxen_user}/.dotfiles"]
-  }
-  file { "/Users/${::boxen_user}/.gitignore":
-    target  => "/Users/${::boxen_user}/.dotfiles/gitignore",
-    require => Repository["/Users/${::boxen_user}/.dotfiles"]
-  }
+# core modules, needed for most things
 
-  include hub
+    include git
+        git::config::global { 'user.email':
+            value  => 'schtoeffel@gmail.com'
+        }
+    file { "/Users/${::boxen_user}/.gitconfig":
+        target  => "/Users/${::boxen_user}/.dotfiles/gitconfig",
+                require => Repository["/Users/${::boxen_user}/.dotfiles"]
+    }
+    file { "/Users/${::boxen_user}/.gitignore":
+        target  => "/Users/${::boxen_user}/.dotfiles/gitignore",
+                require => Repository["/Users/${::boxen_user}/.dotfiles"]
+    }
 
-  # fail if FDE is not enabled
-  if $::root_encrypted == 'no' {
-    fail('Please enable full disk encryption and try again')
-  }
+    include hub
 
-  
-  # common, useful packages
-  package {
-    [
-      'ack',
-      'findutils',
-      'gnu-tar'
-    ]:
-  }
-
-  file { "${boxen::config::srcdir}/our-boxen":
-    ensure => link,
-    target => $boxen::config::repodir
-  }
-
-  $home     = "/Users/${::luser}"
-
-  # .dotfiles
-  $dotfiles  = "${home}/.dotfiles"
-
-  repository { $dotfiles:
-    source  => 'stoeffel/.dotfiles'
-  }
-
-  file { "/Users/${::boxen_user}/Gruntfile.js":
-    target  => "/Users/${::boxen_user}/.dotfiles/Gruntfile.js",
-    require => Repository["/Users/${::boxen_user}/.dotfiles"]
-  }
-
-  # node versions
-  include nodejs::v0_10
-  include nodejs::v0_8_8
-
-  # install some npm modules
-  nodejs::module { 'bower':
-    node_version => 'v0.10'
-  }
-  nodejs::module { 'grunt':
-    node_version => 'v0.10'
-  }
-  nodejs::module { 'grunt-cli':
-    node_version => 'v0.10'
-  }
-  nodejs::module { 'grunt-markdown-pdf':
-    node_version => 'v0.10'
-  }
-  nodejs::module { 'markdown-pdf':
-    node_version => 'v0.10'
-  }
-  nodejs::module { 'yo':
-    node_version => 'v0.10'
-  }
-
-  nodejs::module { 'jshint':
-    node_version => 'v0.10'
-  }
-
-  #nodenv global v0.10
+# fail if FDE is not enabled
+        if $::root_encrypted == 'no' {
+            fail('Please enable full disk encryption and try again')
+        }
 
 
-  # default ruby versions
- ruby::version { '1.9.3': }
-  class { 'ruby::global':
-    version => '1.9.3'
-  }
+# common, useful packages
+    package {
+        [
+            'ack',
+            'findutils',
+            'gnu-tar'
+                ]:
+    }
 
-  ruby::gem { "compass for ${version}":
-    gem =>  'compass',
-    ruby    => '1.9.3'
+    file { "${boxen::config::srcdir}/our-boxen":
+        ensure => link,
+               target => $boxen::config::repodir
+    }
+
+    $home     = "/Users/${::luser}"
+
+# .dotfiles
+        $dotfiles  = "${home}/.dotfiles"
+
+        repository { $dotfiles:
+            source  => 'stoeffel/.dotfiles'
+        }
+
+    file { "/Users/${::boxen_user}/Gruntfile.js":
+        target  => "/Users/${::boxen_user}/.dotfiles/Gruntfile.js",
+                require => Repository["/Users/${::boxen_user}/.dotfiles"]
+    }
+
+# node versions
+    include nodejs::v0_10
+        include nodejs::v0_8_8
+
+# install some npm modules
+        nodejs::module { 'bower':
+            node_version => 'v0.10'
+        }
+    nodejs::module { 'grunt':
+        node_version => 'v0.10'
+    }
+    nodejs::module { 'grunt-cli':
+        node_version => 'v0.10'
+    }
+    nodejs::module { 'grunt-markdown-pdf':
+        node_version => 'v0.10'
+    }
+    nodejs::module { 'markdown-pdf':
+        node_version => 'v0.10'
+    }
+    nodejs::module { 'yo':
+        node_version => 'v0.10'
+    }
+
+    nodejs::module { 'jshint':
+        node_version => 'v0.10'
+    }
+
+#nodenv global v0.10
+
+
+# default ruby versions
+    ruby::version { '1.9.3': }
+    class { 'ruby::global':
+        version => '1.9.3'
+    }
+
+    ruby::gem { "compass for ${version}":
+        gem =>  'compass',
+            ruby    => '1.9.3'
     }
 
 
-  # java
-  include java
+# java
+    include java
 
-  # apache
-  include apache
-  #include php
+# apache
+        include apache
+#include php
 
-  # DB
-  include mysql
-  include mysql_workbench
+# DB
+        include mysql
+        include mysql_workbench
 
-  # browser
-  include chrome
-  include chrome::canary
-  include firefox
+# browser
+        include chrome
+        include chrome::canary
+        include firefox
 
-  # terminal
-  include iterm2::dev
+# terminal
+        include iterm2::dev
 
-  # editors
-  include sublime_text_2
-  sublime_text_2::package { 'JSsnippets':
-    source => 'jprichardson/sublime-js-snippets'
-  }
-  sublime_text_2::package { 'ScssHighlighting':
-    source => 'kuroir/SCSS.tmbundle'
-  }
-  sublime_text_2::package { 'Theme - Flatland':
-    source => 'thinkpixellab/flatland'
-  }
-  sublime_text_2::package { 'Themr':
-    source => 'skt84/Themr'
-  }
-  sublime_text_2::package { 'DocBlockr':
-    source => 'spadgos/sublime-jsdocs'
-  }
-  sublime_text_2::package { 'JsFormat':
-    source => 'jdc0589/JsFormat'
-  }
-  sublime_text_2::package { 'EditorConfig':
-    source => 'sindresorhus/editorconfig-sublime'
-  }
-  sublime_text_2::package { 'Sublime-Hacker-News-Reader':
-    source => 'Dimillian/Sublime-Hacker-News-Reader'
-  }
-  sublime_text_2::package { 'GitGutter':
-    source => 'jisaacks/GitGutter'
-  }
-  sublime_text_2::package { 'CSS-Snippets':
-    source => 'joshnh/CSS-Snippets'
-  }
-  sublime_text_2::package { 'VintageEx':
-    source => 'SublimeText/VintageEx'
-  }
-  
-  
-  
+# editors
+        include sublime_text_2
+        sublime_text_2::package { 'JSsnippets':
+            source => 'jprichardson/sublime-js-snippets'
+        }
+    sublime_text_2::package { 'ScssHighlighting':
+        source => 'kuroir/SCSS.tmbundle'
+    }
+    sublime_text_2::package { 'Theme - Flatland':
+        source => 'thinkpixellab/flatland'
+    }
+    sublime_text_2::package { 'Themr':
+        source => 'skt84/Themr'
+    }
+    sublime_text_2::package { 'DocBlockr':
+        source => 'spadgos/sublime-jsdocs'
+    }
+    sublime_text_2::package { 'JsFormat':
+        source => 'jdc0589/JsFormat'
+    }
+    sublime_text_2::package { 'EditorConfig':
+        source => 'sindresorhus/editorconfig-sublime'
+    }
+    sublime_text_2::package { 'Sublime-Hacker-News-Reader':
+        source => 'Dimillian/Sublime-Hacker-News-Reader'
+    }
+    sublime_text_2::package { 'GitGutter':
+        source => 'jisaacks/GitGutter'
+    }
+    sublime_text_2::package { 'CSS-Snippets':
+        source => 'joshnh/CSS-Snippets'
+    }
+    sublime_text_2::package { 'VintageEx':
+        source => 'SublimeText/VintageEx'
+    }
 
 
-  include eclipse::dev
 
-  include zsh
-  file { "/Users/${::boxen_user}/.zshrc":
-    target  => "/Users/${::boxen_user}/.dotfiles/zshrc",
-    require => Repository["/Users/${::boxen_user}/.dotfiles"]
-  }
-  file { "/Users/${::boxen_user}/.zprofile":
-    target  => "/Users/${::boxen_user}/.dotfiles/zprofile",
-    require => Repository["/Users/${::boxen_user}/.dotfiles"]
-  }
+
+
+    include eclipse::dev
+
+        include zsh
+        file { "/Users/${::boxen_user}/.zshrc":
+            target  => "/Users/${::boxen_user}/.dotfiles/zshrc",
+                    require => Repository["/Users/${::boxen_user}/.dotfiles"]
+        }
+    file { "/Users/${::boxen_user}/.zprofile":
+        target  => "/Users/${::boxen_user}/.dotfiles/zprofile",
+                require => Repository["/Users/${::boxen_user}/.dotfiles"]
+    }
     repository {
-      "/Users/${::boxen_user}/z":
-        source   => 'rupa/z', #short hand for github repos
-        provider => 'git';
+        "/Users/${::boxen_user}/z":
+            source   => 'rupa/z', #short hand for github repos
+            provider => 'git';
     }
 
-  include tmux
-<<<<<<< HEAD
+    include tmux
+        <<<<<<< HEAD
 
-=======
->>>>>>> 23dd322a2b9b6757b621c8db99777d677c6962c9
-  file { "/Users/${::boxen_user}/.tmux.conf":
-    target  => "/Users/${::boxen_user}/.dotfiles/tmux.conf",
-    require => Repository["/Users/${::boxen_user}/.dotfiles"]
-  }
+        =======
+        >>>>>>> 23dd322a2b9b6757b621c8db99777d677c6962c9
+        file { "/Users/${::boxen_user}/.tmux.conf":
+            target  => "/Users/${::boxen_user}/.dotfiles/tmux.conf",
+            require => Repository["/Users/${::boxen_user}/.dotfiles"]
+        }
 
-<<<<<<< HEAD
+    <<<<<<< HEAD
 
-=======
->>>>>>> 23dd322a2b9b6757b621c8db99777d677c6962c9
-  # vim
+        =======
+        >>>>>>> 23dd322a2b9b6757b621c8db99777d677c6962c9
+# vim
 
-  include vim
-  include macvim
-  # Example of how you can manage your .vimrc
-  file { "/Users/${::boxen_user}/.vimrc":
-    target  => "/Users/${::boxen_user}/.dotfiles/vimrc",
-    require => Repository["/Users/${::boxen_user}/.dotfiles"]
-  }
-  file { "/Users/${::boxen_user}/.gvimrc":
-    target  => "/Users/${::boxen_user}/.dotfiles/gvimrc",
-    require => Repository["/Users/${::boxen_user}/.dotfiles"]
-  }
-  file { "/Users/${::boxen_user}/.editorconfig":
-    target  => "/Users/${::boxen_user}/.dotfiles/editorconfig",
-    require => Repository["/Users/${::boxen_user}/.dotfiles"]
-  }
-  vim::bundle { 'gmarik/vundle':}
+        include vim
+        include macvim
+# Example of how you can manage your .vimrc
+        file { "/Users/${::boxen_user}/.vimrc":
+            target  => "/Users/${::boxen_user}/.dotfiles/vimrc",
+            require => Repository["/Users/${::boxen_user}/.dotfiles"]
+        }
+    file { "/Users/${::boxen_user}/.gvimrc":
+        target  => "/Users/${::boxen_user}/.dotfiles/gvimrc",
+        require => Repository["/Users/${::boxen_user}/.dotfiles"]
+    }
+    file { "/Users/${::boxen_user}/.editorconfig":
+        target  => "/Users/${::boxen_user}/.dotfiles/editorconfig",
+        require => Repository["/Users/${::boxen_user}/.dotfiles"]
+    }
+    vim::bundle { 'gmarik/vundle':}
 
-  file { "/Users/${::boxen_user}/.jshintrc":
-    target  => "/Users/${::boxen_user}/.dotfiles/jshintrc",
-    require => Repository["/Users/${::boxen_user}/.dotfiles"]
-  }
+    file { "/Users/${::boxen_user}/.jshintrc":
+        target  => "/Users/${::boxen_user}/.dotfiles/jshintrc",
+                require => Repository["/Users/${::boxen_user}/.dotfiles"]
+    }
 
-  # misc
-  #include sencha_cmd
-  include alfred2
-  include googledrive
-  include sourcetree
-  include vlc
-  include transmission
-  include caffeine
-  include vmware_fusion
-  include shortcat
-  include keyremap4macbook
+# misc
+#include sencha_cmd
+    include alfred2
+        include googledrive
+        include sourcetree
+        include vlc
+        include transmission
+        include caffeine
+        include vmware_fusion
+        include shortcat
+        include keyremap4macbook
 
 
-  # remove
-  package { 'textmate': ensure => absent }
-  package { 'adium': ensure => absent }
-  package { 'textexpander': ensure => absent }
+# remove
+        package { 'textmate': ensure => absent }
+    package { 'adium': ensure => absent }
+    package { 'textexpander': ensure => absent }
 
-  apache::vhost { 'test.dev': 
-    port => 80,
-    docroot  => "${boxen::config::srcdir}/test",
-    host => 'test.dev'
-  }
+    apache::vhost { 'test.dev': 
+        port => 80,
+             docroot  => "${boxen::config::srcdir}/test",
+             host => 'test.dev'
+    }
 
-  apache::vhost { 'boa.dev': 
-    port => 80,
-    docroot  => "${boxen::config::srcdir}/boa",
-    host => 'boa.dev'
-  }
-  apache::vhost { 'fuerto.dev': 
-    port => 80,
-    docroot  => "${boxen::config::srcdir}/fuerto",
-    host => 'fuerto.dev'
-  }
+    apache::vhost { 'boa.dev': 
+        port => 80,
+             docroot  => "${boxen::config::srcdir}/boa",
+             host => 'boa.dev'
+    }
+    apache::vhost { 'fuerto.dev': 
+        port => 80,
+             docroot  => "${boxen::config::srcdir}/fuerto",
+             host => 'fuerto.dev'
+    }
 
-  # OSX - Settings
+# OSX - Settings
 
-  include osx::universal_access::ctrl_mod_zoom
+    include osx::universal_access::ctrl_mod_zoom
 
 }
